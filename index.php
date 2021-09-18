c11bf8102b852939dc75636a3594f351db0bb9b1
getteli <douglas_araujo018@outlook.com>
2021-09-18 18:01:23 -0300

    darkmode

--- index.php
+++ index.php
@@ -19,34 +19,41 @@
 		
 		<title>ilion - localhost</title>
 	</head>
-<body>
-	<div class="container">
-		<div class="m-5">
-			<div class="row justify-content-center">
-				<div class="col text-center">
-					<a href="index.php">
-						<img src="public/image/logoilion.png" alt="ilionTecnologia" width="250px">
-					</a>
+	<body class="">
+		<div class="container">
+
+			<label class="switch">
+				<input type="checkbox" id="dm">
+				<span id="slider" class="slider round"></span>
+			</label>
+
+			<div class="m-5">
+				<div class="row justify-content-center">
+					<div class="col text-center">
+						<a href="index.php">
+							<img src="public/image/logoilion.png" id="logoiliontop" alt="ilion Tecnologia" width="250px">
+						</a>
+					</div>
+				</div>
+
+				<div class="list-group m-5" id="list">
 				</div>
 			</div>
 
-			<div class="list-group m-5" id="list">
+			<div class="footer">
+				<footer class="py-3">
+					<ul id="linef" class="nav justify-content-center border-bottom pb-3 mb-3">
+						<li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+					</ul>
+					<p class="text-center text-muted">
+						<a href="#">
+							© <?=date('Y');?> Ilion tecnologia, powered by - <img src="public/image/logotipoilion.png" id="logoilionbottom" alt="ilion icon">
+						</a>
+					</p>
+				</footer>
 			</div>
 		</div>
-
-		<div class="footer">
-			<footer class="py-3">
-				<ul class="nav justify-content-center border-bottom pb-3 mb-3">
-					<li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
-				</ul>
-				<p class="text-center text-muted">
-					<a href="#">
-						© <?=date('Y');?> Ilion tecnologia, powered by - <img src="public/image/logotipoilion.png" alt="ilion icon">
-					</a>
-				</p>
-			</footer>
-		</div>
-	</div>
-	<script src="public/js/bootstrap.bundle.min.js"></script>
-	<script src="public/js/bootstrap.min.js"></script>
-	<script src="public/js/open.js"></script>
+		
+		<script src="public/js/bootstrap.bundle.min.js"></script>
+		<script src="public/js/bootstrap.min.js"></script>
+		<script src="public/js/open.js"></script>
--- public/js/open.js
+++ public/js/open.js
@@ -1,3 +1,26 @@
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
 function ready() {
 	access();
 }
@@ -26,13 +49,15 @@ function access(subFolder = null, e = null) {
 	.then(function(response) {
 		response.json()
 		.then(function(result) {
+			var darkmode = document.body.classList.contains("dark-mode") ? "dark-mode" : "";
+			var adarkmode = document.body.classList.contains("dark-mode") ? "dark-mode-a" : "";
 			let list = document.getElementById("list");
 			let parent;
 			let nameparent = "";
 			
 			if (subFolder) {
 				var subdiv = document.createElement('div');
-				subdiv.classList = "list-subgroup list-group-item list-group-item-action itens";
+				subdiv.classList = "list-subgroup list-group-item list-group-item-action itens " + darkmode;
 			}
 			if (e != null) {
 				parent = e.parentNode;
@@ -43,20 +68,22 @@ function access(subFolder = null, e = null) {
 			result.forEach(element => {
 				var div = document.createElement('div');
 				if (!subFolder) {
-					div.classList = "parent list-group-item list-group-item-action itens";
+					div.classList = "parent list-group-item list-group-item-action itens " + darkmode;
 				}
 				else{
-					div.classList = "list-group-item list-group-item-action itens";
+					div.classList = "list-group-item list-group-item-action itens " + darkmode;
 				}
+
 				let a = document.createElement('a');
 				
 				a.setAttribute("href", "projects/"+nameparent+element);
+				a.classList = adarkmode;
 				a.setAttribute("target", "_blank");
 				a.append(element);
 				div.appendChild(a);
 				if (!subFolder) {
 					var a2 = document.createElement('a');
-					a2.classList = "arrow iten_a";
+					a2.classList = "arrow iten_a " + adarkmode;
 					a2.setAttribute("href", "#");
 					a2.append("▼");
 					a2.setAttribute("data-name", element);
@@ -78,23 +105,76 @@ function access(subFolder = null, e = null) {
 }
 
 document.body.addEventListener('click', function (e) {
-	if (e.target.className == 'arrow iten_a') {
+	var darkmode = document.body.classList.contains("dark-mode") ? "dark-mode-a" : "";
+	if (e.target.classList.contains("iten_a")) {
 		let itens = document.querySelectorAll("#list a");
-		document.addEventListener("click", function(e) {
+
+		if (!e.target.classList.contains("active")) {
+			let folder = e.target.getAttribute("data-name");
+			e.target.closest("a").classList.add("active");
+			open(folder, e.target);
+		}else{
 			for (let x = 0; x < itens.length; x++) {
 				itens[x].classList.remove("active")
 			}
-			if (e.target != null && e.target.closest("a") != null) {
-				e.target.closest("a").classList.add("active")	
-			}
-		})
-		let folder = e.target.getAttribute("data-name");
-		open(folder, e.target);
+		}
     }
 
 	if (e.target.tagName == 'A' || e.target.tagName == 'DIV') {
 		removeElementsByClass("list-subgroup");
 	}
 }, false);
+
+
+// #region DARK-MODE
+	function darkmode() {
+		var body_ = document.body;
+		var text_ = document.getElementsByTagName('a');
+		var slide_ = document.getElementById("dm");
+		var divs_ = document.getElementsByClassName("itens");
+		var slider_ = document.getElementById("slider");
+		var icontop_ = document.getElementById("logoiliontop");
+		var iconbottom_ = document.getElementById("logoilionbottom");
+		var line_ = document.getElementById("linef");
+
+		body_.classList.toggle("dark-mode");
+		for (const key in text_) {
+			var element = text_[key];
+			if(element.classList){
+				element.classList.toggle("dark-mode-a");
+			}
+			
+		}
+		for (const key in divs_) {
+			var element = divs_[key];
+			if(element.classList){
+				element.classList.toggle("dark-mode");
+			}
+			
+		}
+		if (!body_.classList.contains('dark-mode')) {
+			icontop_.src = "public/image/logoilion.png";
+			iconbottom_.src = "public/image/logotipoilion.png";
+			setCookie("darkmode", false, 999);
+		}else{
+			icontop_.src = "public/image/logoilion_negative.png";
+			iconbottom_.src = "public/image/logotipoilion_negative.png";
+			setCookie("darkmode", true, 999);
+		}
+		line_.classList.toggle("dark-mode");
+		slide_.classList.toggle("dark-mode");
+		slider_.classList.toggle("dark-mode");
+
+	}
+
+	document.getElementById("dm").addEventListener('change', function (e) {
+		darkmode();
+	}, false);
+
+	if (getCookie("darkmode")){
+		darkmode();
+	}
+// #endregion
+
 
 ready();
